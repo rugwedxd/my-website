7a26c1d98146bcefac2c5831dd232f672767cc73
Luffy <rugwedpro@gmail.com>
2026-04-12 01:46:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Website loaded 🚀");
+
+function openPopup(src) {
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popupImg").src = src;
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
