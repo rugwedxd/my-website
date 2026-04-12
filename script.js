673966a3b8e0ea1d43f710ed9efefb150f59b20b
Luffy <rugwedpro@gmail.com>
2026-04-12 15:09:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,47 @@ function closePopup() {
   setTimeout(() => {
     popup.style.display = "none";
   }, 200);
+}const texts = [
+  "I'm a C/C++ Developer",
+  "I'm a Python Developer",
+  "I'm a Video Editor",
+  "I'm a Minecraft Player",
+  "I'm a Student",
+  "I'm a Frontend Developer"
+];
+
+let index = 0;
+let charIndex = 0;
+let currentText = "";
+let isDeleting = false;
+
+function typeEffect() {
+  const typingElement = document.getElementById("typing");
+
+  if (!typingElement) return;
+
+  if (!isDeleting) {
+    currentText = texts[index].substring(0, charIndex++);
+  } else {
+    currentText = texts[index].substring(0, charIndex--);
+  }
+
+  typingElement.textContent = currentText;
+
+  let speed = isDeleting ? 50 : 100;
+
+  if (!isDeleting && charIndex === texts[index].length) {
+    speed = 1500;
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    index = (index + 1) % texts.length;
+    speed = 500;
+  }
+
+  setTimeout(typeEffect, speed);
 }
+
+typeEffect();
+
+
